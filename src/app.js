ae9712ad12adfa9c9fe4f80160168dadef3739cb
Samuel Cha <sunchipssam@gmail.com>
2018-02-16 08:38:38 -0800

    Completed MVP with current and previous day price

--- src/app.js
+++ src/app.js
@@ -7,13 +7,47 @@ const status = config.STATUS;
 
 const port = config.port;
 
-const URL = config.coindesk.URL;
+const currentPriceURL = config.coindesk.URL.currentPrice;
+const yesterdayPriceURL = config.coindesk.URL.previousDayPrice;
 
 const server = express();
 server.use(bodyParser.json());
 
+const cache = {};
+const history = [];
+const capture = {};
+
+const fetchData = (URL, res) => {
+  return fetch(URL)
+    .then(response => response.json())
+    .then(
+      data =>
+        URL.includes('currentprice')
+          ? (capture.current = data.bpi.USD.rate_float)
+          : (capture.previous = Object.values(data.bpi)[0]),
+    )
+    .then(_ => (res ? res.status(status.OK).send(capture) : null))
+    .catch(err => res.status(status.USER_ERROR).send(err));
+};
+
 server.get('/compare', (req, res) => {
-  res.send('compare');
+  fetchData(currentPriceURL).then(_ => {
+    fetchData(yesterdayPriceURL, res);
+  });
+});
+
+server.get('/c', (req, res) => {
+  fetch(currentPriceURL)
+    .then(response => response.json())
+    .then(data => res.status(200).send(data))
+    .catch(err => res.status(422).send(err));
+});
+
+server.get('/c1', (req, res) => {
+  fetch(yesterdayPriceURL)
+    .then(response => response.json())
+    .then(data => res.status(200).send(data))
+    .catch(err => res.status(422).send(err));
 });
 
 server.listen(port);
